811eb101ae1e02d6fffc7b98c0f0a31e9d87cc30
MartaSzuran <marta.szuran@gmail.com>
2022-06-07 15:54:11 +0200

    Working_with_input_values_into_observation_form_missing_date

--- app/components/observation-form/component.js
+++ app/components/observation-form/component.js
@@ -11,6 +11,54 @@ export default class ObservationFormComponent extends Component {
   @tracked notes = '';
 
   get isEmptyField() {
-    return !(this.date, this.birdname, this.place, this.notes);
+    return !(this.birdname, this.place, this.notes);
+  }
+
+  @action
+  onDateChange() {
+    this.date = document.getElementById('datepicker');
+    console.log(this.date);
+  }
+
+  @action
+  onBirdnameChange(event) {
+    this.birdname = event.target.value;
+    console.log(this.birdname);
+  }
+
+  @action
+  onPlaceChange(event) {
+    this.place = event.target.value;
+    console.log(this.place);
+  }
+
+  @action
+  onNotesChange(event) {
+    this.notes = event.target.value;
+    console.log(this.notes);
+  }
+
+  @action
+  async onSave() {
+    const observation = {
+      date: this.date,
+      birdname: this.birdname,
+      place: this.place,
+      notes: this.notes,
+    };
+
+    const observationModel = this.store.createRecord(
+      'observation',
+      observation
+    );
+    await observationModel.save();
+    this.clear();
+  }
+
+  clear() {
+    this.date = '';
+    this.birdname = '';
+    this.place = '';
+    this.notes = '';
   }
 }
