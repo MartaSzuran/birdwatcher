001497dc3d1ee31d1bc512f8aa5cb7079e4ca658
MartaSzuran <marta.szuran@gmail.com>
2022-06-10 10:53:35 +0200

    Adding_logic_to_loginIn_and_logout/_addding_home_route_with_generalContainer_and_navbar/_adding_session_service

--- app/home/route.js
+++ app/home/route.js
@@ -1,16 +1,16 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class LoginRoute extends Route {
+export default class HomeRoute extends Route {
   @service router;
   @service session;
 
-  beforeModel() {
+  async beforeModel() {
     const { isUserLoggedIn } = this.session;
-
-    if (isUserLoggedIn) {
-      this.router.transitionTo('home');
+    if (!isUserLoggedIn) {
+      this.router.transitionTo('login');
       return;
     }
+    await this.session.setCurrentUser();
   }
 }
--- app/login/controller.js
+++ app/login/controller.js
@@ -1,19 +1,28 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginController extends Controller {
+  @service store;
+  @service session;
+  @tracked userName;
+  @tracked userPassword;
+
   @action
-  onLoginChange() {
-    console.log('login change');
+  onLoginChange(event) {
+    this.userName = event.target.value;
   }
 
   @action
-  onPasswordChange() {
-    console.log('password change');
+  onPasswordChange(event) {
+    this.userPassword = event.target.value;
   }
 
   @action
-  onSubmit() {
-    console.log('submit change');
+  async onSubmit(event) {
+    event.preventDefault();
+    const { userName, userPassword } = this;
+    await this.session.loggedUser(userName, userPassword);
   }
 }
