b7be0b2443b1e4e2f3bf0cd6d8d1f12cb59cd675
MartaSzuran <marta.szuran@gmail.com>
2022-06-11 08:12:08 +0200

    Adding_user_observation_using_session

--- app/home/user/route.js
+++ app/home/user/route.js
@@ -3,9 +3,10 @@ import { inject as service } from '@ember/service';
 
 export default class UserRoute extends Route {
   @service store;
+  @service session;
 
   model() {
-    const user = this.store.findRecord('user', 1);
+    const user = this.session.currentUser;
     return user;
   }
 }
