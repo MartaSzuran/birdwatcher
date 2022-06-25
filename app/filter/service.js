91acecc17908e4f51638070172dbfba7e987f4ea
MartaSzuran <marta.szuran@gmail.com>
2022-06-25 08:10:46 +0200

    Fix_storing_datas_in_service_filter

--- app/filter/service.js
+++ app/filter/service.js
@@ -7,14 +7,18 @@ import moment from 'moment';
 export default class FilterDatesService extends Service {
   @service store;
   @storageFor('filter-dates') filterDates;
+  @tracked startDate;
+  @tracked endDate;
 
   setStartDate(date) {
     this.filterDates.set('dateStart', moment(date).format('YYYY-MM-DD'));
+    const startDate = this.filterDates.get('dateStart');
     console.log(this.filterDates.get('dateStart'));
   }
 
   setEndDate(date) {
     this.filterDates.set('dateEnd', moment(date).format('YYYY-MM-DD'));
+    const endDate = this.filterDates.get('dateEd');
     console.log(this.filterDates);
   }
 
