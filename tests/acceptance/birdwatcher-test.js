b74c3d581b3120983228ac87396a6726a333c4b0
MartaSzuran <marta.szuran@gmail.com>
2022-07-02 13:54:55 +0200

    Preparing_for_test

--- tests/acceptance/birdwatcher-test.js
+++ tests/acceptance/birdwatcher-test.js
@@ -5,14 +5,14 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | birdwatcher', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /', async function (assert) {
+  test.skip('visiting /', async function (assert) {
     await visit('/');
 
     assert.strictEqual(currentURL(), '/');
     assert.dom('ul').exists();
   });
 
-  test('navigating using the navbar', async function (assert) {
+  test.skip('navigating using the navbar', async function (assert) {
     await visit('/');
 
     assert.dom('ul').exists();
