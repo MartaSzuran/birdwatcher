d98da7a4ca7be273030ab377f458470cc63e589b
MartaSzuran <marta.szuran@gmail.com>
2022-07-05 14:24:24 +0200

    Getting_to_the_lat_ad_lng_of_click_event

--- app/components/my-map/component.js
+++ app/components/my-map/component.js
@@ -4,6 +4,7 @@ import { tracked } from '@glimmer/tracking';
 
 export default class MyMapComponent extends Component {
   @tracked isOpenLocationId = null;
+  @tracked position;
 
   @action
   markerTooltipOpen(location) {
@@ -11,5 +12,17 @@ export default class MyMapComponent extends Component {
       return (this.isOpenLocationId = null);
     }
     this.isOpenLocationId = location.get('id');
+  }
+
+  @action
+  didLoadMap(event) {
+    const myNewmap = event;
+    console.log(myNewmap);
+  }
+
+  @action
+  onClick(event) {
+    const myNewEventObj = event;
+    console.log(myNewEventObj.googleEvent.xb.x);
   }
 }
