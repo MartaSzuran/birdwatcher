c77e3823eef1eab9388201c2c4b96bc4efde19b6
MartaSzuran <marta.szuran@gmail.com>
2022-07-07 15:54:09 +0200

    Taking_bounds_of_map_but_cannot_print_sutable_markers

--- app/components/my-map/show-all-observations/component.js
+++ app/components/my-map/show-all-observations/component.js
@@ -3,9 +3,41 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
-export default class MyMapShowAllObservationsComponent extends Component {
+export default class MyMapShowAllObservationsObservationOnMapTableComponent extends Component {
   @service store;
   @tracked isOpenObservationId = null;
+  @tracked map;
+  @tracked latBounds;
+  @tracked lngBounds;
+
+  get currentMapObservations() {
+    let observations = this.args.observations;
+    observations = observations.filter((element) => {
+      this.checkLocation(element);
+    });
+    return observations;
+  }
+
+  checkLocation(element) {
+    if (
+      element.latLocation >= this.latBounds[0] &&
+      element.latLocation <= this.latBounds[1] &&
+      element.lngLocation >= this.lngBounds[0] &&
+      element.lngLocation <= this.lngBounds[1]
+    ) {
+      return element;
+    }
+  }
+
+  @action
+  saveBounds(event) {
+    let bounds = event.map.getBounds();
+    // console.log(bounds);
+    this.latBounds = [bounds.wb.lo, bounds.wb.hi];
+    this.lngBounds = [bounds.Ra.lo, bounds.Ra.hi];
+    // console.log(this.latBounds);
+    // console.log(this.lngBounds);
+  }
 
   @action
   markerTooltipOpen(observation) {
