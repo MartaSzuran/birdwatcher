84567b21e13a124a2d7e66cd9c8824093799acc1
MartaSzuran <marta.szuran@gmail.com>
2022-07-16 21:59:34 +0200

    Fix_with_npm_run_lint

--- tests/integration/components/show-observations/component-test.js
+++ tests/integration/components/show-observations/component-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import moment from 'moment';
