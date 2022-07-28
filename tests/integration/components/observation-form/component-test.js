a02e051213fb4eb23ec77f71e2578b5e2be9c5dd
MartaSzuran <marta.szuran@gmail.com>
2022-07-28 11:46:14 +0200

    Finishing_tests_for_form_observation

--- tests/integration/components/observation-form/component-test.js
+++ tests/integration/components/observation-form/component-test.js
@@ -1,15 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, fillIn } from '@ember/test-helpers';
+import {
+  render,
+  click,
+  fillIn,
+  findAll,
+  find,
+  waitFor,
+} from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { Interactor as Pikaday } from 'ember-pikaday/test-support';
 import { close as closePikaday } from 'ember-pikaday/test-support';
 import moment from 'moment';
+import { setupMapTest } from 'ember-google-maps/test-support';
 
 module('Integration | Component | observation-form', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
+  setupMapTest(hooks);
 
   hooks.beforeEach(async function () {
     const observation = this.server.create('observation');
@@ -59,24 +68,18 @@ module('Integration | Component | observation-form', function (hooks) {
 
     await fillIn('[data-test-input-birdname]', `${this.observation.birdname}`);
     await fillIn('[data-test-input-notes]', `${this.observation.notes}`);
-    await fillIn(
-      '[data-test-input-location]',
-      `latitude: ${this.observation.latLocation.toFixed(
-        6
-      )} / longitude: ${this.observation.lngLocation.toFixed(6)}`
-    );
+    await click('[data-test-input-location]');
+
+    const { x, y, width, height } = find('iframe').getBoundingClientRect();
+    document.elementFromPoint(x + width / 2, y + height / 2).click();
 
     assert.dom('[data-test-input-pikaday]').hasValue(stringDate);
     assert
       .dom('[data-test-input-birdname]')
       .hasValue(`${this.observation.birdname}`);
-    assert
-      .dom('[data-test-input-location]')
-      .hasValue(
-        `latitude: ${this.observation.latLocation.toFixed(
-          6
-        )} / longitude: ${this.observation.lngLocation.toFixed(6)}`
-      );
+    await waitFor('[data-test-input-location]', { timeout: 5000 });
+    const inputArr = findAll('input');
+    assert.dom('[data-test-input-location]').hasValue(`${inputArr[2].value}`);
     assert.dom('[data-test-input-notes]').hasValue(`${this.observation.notes}`);
   });
 
