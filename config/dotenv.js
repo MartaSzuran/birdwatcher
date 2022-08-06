851fdb0f97276fe3fffd688846b72448e185b36c
MartaSzuran <marta.szuran@gmail.com>
2022-08-06 11:53:40 +0200

    Env changes for netlify

--- config/dotenv.js
+++ config/dotenv.js
@@ -2,11 +2,12 @@
 
 const path = require('path');
 
-module.exports = function () {
+module.exports = function (env) {
   return {
     clientAllowedKeys: ['GOOGLE_MAPS_API_KEY'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env'),
+    enabled: env !== 'production',
   };
 };
