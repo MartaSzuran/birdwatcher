0dc49f35f9feba64cafbb3b23f99466bd77234b2
MartaSzuran <marta.szuran@gmail.com>
2022-08-08 13:02:12 +0200

    Testing map from observation form

---
+++ tests/integration/components/my-map/add-observation/component-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { setupMapTest, waitForMap } from 'ember-google-maps/test-support';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Acceptance | Component | my-map/add-observation ', function (hooks) {
+  setupRenderingTest(hooks);
+  setupMapTest(hooks);
+
+  test('check if map instance', async function (assert) {
+    await render(hbs`<ObservationForm::MapModal
+    @onLatLocationChange={{this.onLatLocationChange}}
+    @onLngLocationChange={{this.onLngLocationChange}}
+    @hideMapModal={{this.hideMapModal}}
+    />`);
+    const { map } = await waitForMap();
+    assert.ok(map);
+    assert.dom('[data-test-map]').exists();
+  });
+});
