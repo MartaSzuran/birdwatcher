5b5b34967eb907dd62e643902dc52a46f94460c9
MartaSzuran <marta.szuran@gmail.com>
2022-08-21 15:42:03 +0200

    Acceptane test home - navigating using navbar

---
+++ tests/acceptance/home-test.js
@@ -0,0 +1,57 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Acceptance | Home', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function () {
+    this.sessionService = this.owner.lookup('service:session');
+    const store = await this.owner.lookup('service:store');
+
+    const loggedUser = await this.server.create('user');
+    const loggedUserModel = await store.findRecord('user', loggedUser.id);
+
+    this.set('store', store);
+    this.set('sessionService.currentUser', loggedUserModel);
+
+    window.localStorage.setItem(
+      'storage:logged-as',
+      JSON.stringify({ id: loggedUserModel.id })
+    );
+  });
+
+  test('navigating using the navbar', async function (assert) {
+    await visit('/');
+
+    assert.dom('[data-test-navbar-bird-image]').exists();
+    assert
+      .dom('[data-test-navbar-bird-image]')
+      .hasAttribute('src', '/assets/images/bird.png');
+
+    assert.dom('[data-test-navbar-profile]').exists();
+    assert.dom('[data-test-navbar-profile]').hasText('Profile');
+    await click('[data-test-navbar-profile]');
+    assert.strictEqual(currentURL(), '/user');
+
+    assert.dom('[data-test-navbar-observations]').exists();
+    assert.dom('[data-test-navbar-observations]').hasText('Observations');
+    await click('[data-test-navbar-observations]');
+    assert.strictEqual(currentURL(), '/observations');
+
+    assert.dom('[data-test-navbar-add-observation]').exists();
+    assert.dom('[data-test-navbar-add-observation]').hasText('Add observation');
+    await click('[data-test-navbar-add-observation]');
+    assert.strictEqual(currentURL(), '/add-observation');
+
+    assert.dom('[data-test-navbar-home]').exists();
+    assert.dom('[data-test-navbar-home]').hasText('Home');
+    await click('[data-test-navbar-home]');
+    assert.strictEqual(currentURL(), '/');
+
+    assert.dom('[data-test-navbar-logout]').exists();
+    assert.dom('[data-test-navbar-logout]').hasText('Logout');
+  });
+});
